6b49de0735be2d81db4ffdcf744429248ef81917
Timo Korthals <tkorthals@techfak.uni-bielefeld.de>
2015-01-31 02:22:04 +0100

    watchdog: Enable TWL4030 watchdog and added module parameter pingonprobe for twl4030_wdt

--- recipes-support/watchdog-sample/watchdog-sample/watchdog-sample.c
+++ recipes-support/watchdog-sample/watchdog-sample/watchdog-sample.c
@@ -23,15 +23,6 @@
  * THE SOFTWARE.
  */
 
-/*
- * This file is a simple example on the use of the systemd SW watchdog.
- * At startup, the program will create a file in /tmp/watchdog-sample.tmp;
- * if the file is removed while the program is running, the program will not
- * ping systemd watchdog, resulting in a restart of the program (depending on
- * the service configuration).
- * This file allow to simulate a hang in the watchdog-critical program.
- */
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
@@ -45,7 +36,7 @@ int main(int argc, char * argv[]) {
 	uint32_t wd_timeout; // in seconds
 	uint32_t sleep_time;
 	int err;
-	char path[] = "/tmp/watchdog-sample.tmp";
+	char path[] = "/dev/watchdog";
 	int fd;
 	if(argc == 2) {
 		wd_timeout = atoi(argv[1]);
@@ -65,17 +56,14 @@ int main(int argc, char * argv[]) {
 
 	fd = open(path, O_RDWR | O_CREAT | O_SYNC, 777);
 	if(fd == -1) {
-		sd_notifyf(0, SD_EMERG "STATUS=Failed to create file: %s\n"
+		sd_notifyf(0, SD_EMERG "STATUS=Failed to open watchdog device: %s\n"
 			"ERRNO=%i",
 			strerror(errno), errno);
 		exit(-1);
 	}
 	write(fd,"A", 1);
 	fsync(fd);
-	close(fd);
 
-	/* Here, systemd WD is enabled and waiting for ping */
-	/* wd_timeout variable contains the timeout. We need to ping every wd_timeout/2 */
 	sleep_time = wd_timeout/2;
 	sd_notify(0, "READY=1\n");
 
@@ -84,9 +72,12 @@ int main(int argc, char * argv[]) {
 		sleep(sleep_time);
 
 		if(access(path, F_OK) == -1) {
-			sd_notify(0, SD_EMERG "TEMP file disappeared, falling in hang mode\n");
+			sd_notify(0, SD_EMERG "Watchdog device file disappeared, falling in hang mode\n");
 			while(1);
 		}
+		write(fd,"A", 1);
+		fsync(fd);
 	}
+        close(fd);
 }
 
